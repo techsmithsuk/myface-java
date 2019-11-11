0fa525eed0ff3da973e434f57f6a746e40bd4038
Mike <Walker>
2019-11-11 23:29:51 +0000

    Add user view model.

--- src/main/java/techsmiths/myface/models/viewmodels/UserViewModel.java
+++ src/main/java/techsmiths/myface/models/viewmodels/UserViewModel.java
@@ -1,4 +1,39 @@
 package techsmiths.myface.models.viewmodels;
 
+import techsmiths.myface.models.dbmodels.User;
+
 public class UserViewModel {
+    User user;
+
+    public UserViewModel(User user) {
+        this.user = user;
+    }
+
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    public String getEmail() {
+        return user.getEmail();
+    }
+
+    public String getFirstName() {
+        return user.getFirstName();
+    }
+
+    public String getLastName() {
+        return user.getLastName();
+    }
+
+    public String getDisplayName() {
+        return String.format("%s %s", user.getFirstName(), user.getLastName());
+    }
+
+    public String getProfileImage() {
+        return user.getProfileImage();
+    }
+
+    public String getBannerImage() {
+        return user.getBannerImage();
+    }
 }
