be72c388d3b92f4105904766b47eef75687e62a6
Mike <Walker>
2019-11-11 23:01:10 +0000

    Add base database service.

---
+++ src/main/java/techsmiths/myface/services/DatabaseService.java
@@ -0,0 +1,24 @@
+package techsmiths.myface.services;
+
+import org.jdbi.v3.core.Jdbi;
+
+public abstract class DatabaseService {
+    protected final Jdbi jdbi;
+
+    protected DatabaseService() {
+        String hostname = "localhost";
+        String databaseName = System.getenv("DB_NAME");
+        String port = "3306";
+        String username = System.getenv("DB_USERNAME");;
+        String password = System.getenv("DB_PASSWORD");;
+
+        String connectionString = "jdbc:mysql://" + hostname +
+                ":" + port +
+                "/" + databaseName +
+                "?user=" + username +
+                "&password=" + password +
+                "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
+
+        jdbi = Jdbi.create(connectionString);
+    }
+}
