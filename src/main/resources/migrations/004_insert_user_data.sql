913a6dc455ac64dbf5e6484903aea7f1bed818cb
Mike <Walker>
2019-11-12 08:10:14 +0000

    Add data for users.

---
+++ src/main/resources/migrations/004_insert_user_data.sql
@@ -0,0 +1,18 @@
+INSERT INTO users
+(user_name, email, first_name, last_name, profile_image, banner_image)
+VALUES
+("rabram0", "rabram0@furl.net", "Rois", "Abram", "https://picsum.photos/id/100/400/400", "https://picsum.photos/id/200/1600/400"),
+("kcoppin1", "kcoppin1@ifeng.com", "Keane", "Coppin", "https://picsum.photos/id/101/400/400", "https://picsum.photos/id/201/1600/400"),
+("wdensey2", "wdensey2@zimbio.com", "Wendell", "Densey", "https://picsum.photos/id/102/400/400", "https://picsum.photos/id/202/1600/400"),
+("hshillito3", "hshillito3@deviantart.com", "Hammad", "Shillito", "https://picsum.photos/id/103/400/400", "https://picsum.photos/id/203/1600/400"),
+("callan4", "callan4@pbs.org", "Carleen", "Allan", "https://picsum.photos/id/104/400/400", "https://picsum.photos/id/204/1600/400"),
+("sdinwoodie5", "sdinwoodie5@digg.com", "Sofia", "Dinwoodie", "https://picsum.photos/id/105/400/400", "https://picsum.photos/id/205/1600/400"),
+("lluttger6", "lluttger6@berkeley.edu", "Launce", "Luttger", "https://picsum.photos/id/106/400/400", "https://picsum.photos/id/206/1600/400"),
+("bwedgwood7", "bwedgwood7@sohu.com", "Billi", "Wedgwood", "https://picsum.photos/id/100/407/400", "https://picsum.photos/id/207/1600/400"),
+("kskentelbury8", "kskentelbury8@w3.org", "Kaye", "Skentelbury", "https://picsum.photos/id/108/400/400", "https://picsum.photos/id/208/1600/400"),
+("asingh9", "asingh9@wikia.com", "Alfreda", "Singh", "https://picsum.photos/id/109/400/400", "https://picsum.photos/id/209/1600/400"),
+("hdiesa", "hdiesa@opensource.org", "Hans", "Dies", "https://picsum.photos/id/110/400/400", "https://picsum.photos/id/210/1600/400"),
+("amulvennab", "amulvennab@umich.edu", "Aurea", "Mulvenna", "https://picsum.photos/id/111/400/400", "https://picsum.photos/id/211/1600/400"),
+("jforsaithc", "jforsaithc@aboutads.info", "Janis", "Forsaith", "https://picsum.photos/id/112/400/400", "https://picsum.photos/id/212/1600/400"),
+("drowlerd", "drowlerd@altervista.org", "Davidson", "Rowler", "https://picsum.photos/id/113/400/400", "https://picsum.photos/id/213/1600/400"),
+("cfidocke", "cfidocke@reverbnation.com", "Cornelius", "Fidock", "https://picsum.photos/id/114/400/400", "https://picsum.photos/id/214/1600/400");
