bdcb07ad05f99218b7d219f27600883bb109c252
Mike <Walker>
2019-11-12 07:47:22 +0000

    Include User with All Posts.

---
+++ src/main/java/techsmiths/myface/models/dbmodels/Sender.java
@@ -0,0 +1,5 @@
+package techsmiths.myface.models.dbmodels;
+
+public class Sender extends User {
+    // Separate Sender and Receiver classes needed to ease JDBI mapping.
+}
--- src/main/java/techsmiths/myface/services/PostService.java
+++ src/main/java/techsmiths/myface/services/PostService.java
@@ -1,20 +1,42 @@
 package techsmiths.myface.services;
 
+import org.jdbi.v3.core.mapper.reflect.BeanMapper;
 import org.springframework.stereotype.Service;
 import techsmiths.myface.models.dbmodels.Post;
+import techsmiths.myface.models.dbmodels.Receiver;
+import techsmiths.myface.models.dbmodels.Sender;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService extends DatabaseService {
 
     public List<Post> getAllPosts(int limit, int offset) {
         return jdbi.withHandle(handle ->
-                handle.createQuery("SELECT * FROM posts LIMIT :limit OFFSET :offset")
+                new ArrayList<>(handle.createQuery("" +
+                            "SELECT * FROM posts as post " +
+                            "JOIN users as sender on posts.sender_user_id = user.id " +
+                            "JOIN users as receiver on posts.receiver_user_id = user.id " +
+                            "LIMIT :limit OFFSET :offset")
                         .bind("limit", limit)
                         .bind("offset", offset)
-                        .mapToBean(Post.class)
-                        .list()
+                        .registerRowMapper(BeanMapper.factory(Post.class, "post"))
+                        .registerRowMapper(BeanMapper.factory(Sender.class, "sender"))
+                        .registerRowMapper(BeanMapper.factory(Receiver.class, "receiver"))
+                        .reduceRows(new LinkedHashMap<Integer, Post>(), (map, row) -> {
+                            Post post = map.computeIfAbsent(
+                                    row.getColumn("post.id", Integer.class), id -> row.getRow(Post.class)
+                            );
+
+                            post.setSender(row.getRow(Sender.class));
+                            post.setSender(row.getRow(Receiver.class));
+
+                            return map;
+                        })
+                        .values())
         );
     }
 
