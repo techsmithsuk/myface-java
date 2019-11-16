91fc311cb247941a234f3713c2027a2318086829
Mike <Walker>
2019-11-16 09:11:40 +0000

    Improve initial posted at data.

--- src/main/resources/migrations/006_add_dates_for_posts.sql
+++ src/main/resources/migrations/006_add_dates_for_posts.sql
@@ -2,7 +2,7 @@
 ALTER TABLE posts ADD COLUMN posted_at datetime;
 
 # Not ideal - set the date to now for all pre-existing posts.
-UPDATE posts SET posted_at = NOW();
+UPDATE posts SET posted_at = DATE_ADD(NOW(), INTERVAL FLOOR(RAND() * -10000) SECOND) WHERE posts.posted_at is NULL;
 
 # Finally make the column not-null, so that in the future all posts must have a datetime.
 ALTER TABLE posts MODIFY COLUMN posted_at datetime NOT NULL;
